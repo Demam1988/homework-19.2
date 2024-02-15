6c490c7494987fa82d7a5a44df5a31c006a0bb43
Mamed Mamedov <demam777@gmail.com>
2024-02-15 10:30:49 +0300

    url

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
 from catalog.views import home, contacts
 
 urlpatterns = [
     path('', home, name='catalog'),
-    path('', contacts, name='catalog')
+    path('contacts/', contacts, name='catalog')
 ]

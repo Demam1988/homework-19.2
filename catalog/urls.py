18a7cc8fc3008f9d30132c22461d96cd39edb4bf
Mamed Mamedov <demam777@gmail.com>
2024-03-07 12:21:53 +0300

    20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,13 +6,7 @@ from catalog.models import Product, Category
 from catalog.views import home, contacts, product_list, product_detail
 
 urlpatterns = [
-    path('', home, name='catalog'),
-    path('contacts/', contacts, name='contacts'),
-    path('contacts/', Category, name='category'),
-    path('contacts/', Product, name='products'),
     path('', product_list, name='product_list'),
 #    path('catalog/<pk:int>/', product_detail, name='product_detail')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
 

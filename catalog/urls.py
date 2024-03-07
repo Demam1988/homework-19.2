63679b475454595f7ea841e6e061382e5fd40e3e
Mamed Mamedov <demam777@gmail.com>
2024-03-07 17:10:23 +0300

    20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,18 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from catalog.apps import CatalogConfig
 
 from catalog.models import Product, Category
 from catalog.views import home, contacts, product_list, product_detail
 
+app_name = CatalogConfig.name
+
+
 urlpatterns = [
-    path('', product_list, name='product_list'),
-#    path('catalog/<pk:int>/', product_detail, name='product_detail')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('', product_list, name='product_list'),
+                  path('products', product_list, name='product_list'),
+ #                 path('product/<ink:pk>/', product_detail, name='product_detail')
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
--- config/urls.py
+++ config/urls.py
@@ -19,8 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('catalog.urls'), name='catalog')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', include('catalog.urls', namespace='catalog'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
